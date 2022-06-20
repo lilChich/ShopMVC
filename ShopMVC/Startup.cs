ef9f30347f8ee7da2f9eda61a316a0d5fbcf1d9e
WhoDatBoi <bohdan.butenko@nure.ua>
2022-06-20 18:21:07 +0300

    Basket view was added + style.css + images folder

--- ShopMVC/Startup.cs
+++ ShopMVC/Startup.cs
@@ -1,12 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using ShopMVC.BLL.Infrastructure.Security;
+using ShopMVC.BLL.Interfaces.IServices;
+using ShopMVC.BLL.Interfaces.JWT;
+using ShopMVC.BLL.Services;
+using ShopMVC.DAL;
+using ShopMVC.DAL.Entities;
+using ShopMVC.DAL.Interfaces;
+using ShopMVC.DAL.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ShopMVC
@@ -23,6 +37,61 @@ namespace ShopMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(Assembly.Load("ShopMVC"), Assembly.Load("ShopMVC.DAL"));
+            services.AddAutoMapper(Assembly.Load("ShopMVC.BLL"), Assembly.Load("ShopMVC.DAL"));
+
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.User.AllowedUserNameCharacters = null;
+                options.SignIn.RequireConfirmedAccount = false;
+
+            })
+                .AddEntityFrameworkStores<DataContext>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredUniqueChars = 1;
+
+                options.Lockout.MaxFailedAccessAttempts = 3;
+
+                options.User.AllowedUserNameCharacters = null;
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.AddScoped<IAuthService, AuthorizationService>();
+
+            services.AddScoped<IUnitOfWork, EFUnitOfWork>();
+            services.AddScoped<UserManager<ApplicationUser>>();
+            services.AddScoped<SignInManager<ApplicationUser>>();
+            services.AddScoped<RoleManager<ApplicationRole>>();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(
+                    opt =>
+                    {
+                        opt.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = key,
+                            ValidateAudience = false,
+                            ValidateIssuer = false
+                        };
+                    }
+                );
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
+
             services.AddControllersWithViews();
         }
 
@@ -44,13 +113,17 @@ namespace ShopMVC
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
